f48981689b6f4bb1964d7203f0c5b65cb2b75d75
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-02-17 16:01:25 +0000

    Some cleanup

--- Client.java
+++ Client.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.*;
 
 public class Client {
 
@@ -9,12 +7,11 @@ public class Client {
 
 
 	public static void main(String[] args) {
-		if(args.length != 4 && args.length != 5)
-		{
+		if(args.length != 4 && args.length != 5) {
 			System.out.println("Usage: java Client <hostname> <portnumber> <operation> <operands> [operand]");
 			return;
 		}
-		
+
 		String hostName = args[0];
 		int serverPort = Integer.parseInt(args[1]);
 
@@ -23,28 +20,22 @@ public class Client {
 		DatagramPacket msgToSend;
 		try {
 			socket = new DatagramSocket();
-			
+
 			//TODO Modificar a chamada dos DatagramPacket para incluir address 
 			msgReceived = new DatagramPacket(new byte[UDP_DATAGRAM_MAX_LENGTH], UDP_DATAGRAM_MAX_LENGTH);
 			msgToSend = new DatagramPacket(new byte[UDP_DATAGRAM_MAX_LENGTH], UDP_DATAGRAM_MAX_LENGTH);
-			
-			//TEMPORARY
+
 			String str = args[2];
 			for(int i = 3; i < args.length; i++)
-			{
 				str += " " + args[i];
-				
-			}
 			msgToSend.setData(str.getBytes());
-			
+
 			socket.send(msgToSend);
 			socket.close();
 			//socket·;
 		} catch (SocketException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
--- Server.java
+++ Server.java
@@ -7,44 +7,38 @@ import java.net.SocketException;
 public class Server {
 
 	public static final int UDP_DATAGRAM_MAX_LENGTH = 65536; //2^16
-	
+
 	public static void main(String[] args) {
-		
-		if(args.length != 1)
-		{
+
+		if(args.length != 1) {
 			System.out.println("Usage: java Server <port number>");
 			return;
 		}
-		
+
 		int serverPort = Integer.parseInt(args[0]);
-		
+
 		DatagramSocket socket;
 		DatagramPacket msgReceived;
 		DatagramPacket msgToSend;
 		try {
 			socket = new DatagramSocket(serverPort);
-			
-			while(true)
-			{
+
+			while(true) {
 				msgReceived = new DatagramPacket(new byte[UDP_DATAGRAM_MAX_LENGTH], UDP_DATAGRAM_MAX_LENGTH);
 				msgToSend = new DatagramPacket(new byte[UDP_DATAGRAM_MAX_LENGTH], UDP_DATAGRAM_MAX_LENGTH);
-						
+
 				socket.receive(msgReceived);
-				
+
 				//Processing the message
 				System.out.println(msgReceived.getData());
 			}
-			
+
 			//socket·;
+			//socket.close();
 		} catch (SocketException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally
-		{
-			//TODO socket.close();
 		}
 
 	}
