6af1c41f86f82337fe27ea739b1b92e9045b7678
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-02-28 00:26:24 +0000

    Server multithreading implemented

--- Server.java
+++ Server.java
@@ -1,16 +1,61 @@
 package SDIS;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.SocketException;
+import java.net.*;
 import java.util.ArrayList;
+import java.util.TimerTask;
 
 public class Server {
 
 	public static final int UDP_DATAGRAM_MAX_LENGTH = 65536; //2^16
+	
+	private static MulticastSocket generateSocket() throws IOException {
+		MulticastSocket socket = new MulticastSocket();
+		socket.setTimeToLive(1); //To avoid network congestion
+		return socket;
+	}
+	
+	private static void advertiser(int servicePort, InetAddress multicastAddress, int multicastPort) {
+		try {
+			//Prepare the multicast message to diffuse
+			StringBuilder mcastMessage = new StringBuilder("multicast:");
+			mcastMessage.append(InetAddress.getLocalHost().getHostAddress()).append(" ").append(servicePort);
+			String mcastMsg = mcastMessage.toString();
+
+			MulticastSocket socket = generateSocket();
+			byte[] data = new byte[UDP_DATAGRAM_MAX_LENGTH];
+			DatagramPacket msgToDiffuse = new DatagramPacket(data, data.length, multicastAddress, multicastPort);
+			msgToDiffuse.setData(mcastMsg.getBytes());
+
+			while(true) {
+				socket.send(msgToDiffuse);
+				System.out.println(mcastMsg);
+				//Just for avoiding overflood
+				Thread.sleep(1000);
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void requestsProcessor() {
+		ArrayList<Plate> plateList = new ArrayList<>();
+
+		try {
+			MulticastSocket socket = generateSocket();
+			byte[] data = new byte[UDP_DATAGRAM_MAX_LENGTH];
+
+			while(true) {
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
 		if(args.length != 3) {
@@ -18,39 +63,26 @@ public class Server {
 			return;
 		}
 
-		ArrayList<Plate> plateList = new ArrayList<>();
-		int serverPort = Integer.parseInt(args[0]);
-
 		try {
-			MulticastSocket socket = new MulticastSocket();
-			socket.setTimeToLive(1); //To avoid network congestion
-			byte[] data = new byte[UDP_DATAGRAM_MAX_LENGTH];
+			int servicePort = Integer.parseInt(args[0]);
+			InetAddress multicastAddress = InetAddress.getByName(args[1]);
+			int multicastPort = Integer.parseInt(args[2]);
 
-			int i = 0;
-			while(true) {
-				//Prepare the multicast message to diffuse
-				InetAddress multicastAddress = InetAddress.getByName(args[1]);
-				int multicastPort = Integer.parseInt(args[2]);
-				DatagramPacket msgToDiffuse = new DatagramPacket(data, data.length, multicastAddress, multicastPort);
-				
-				StringBuilder mcastMessage = new StringBuilder("multicast:");
-				mcastMessage.append(InetAddress.getLocalHost().getHostAddress()).append(" ").append(args[0]);
-				
-				
-				msgToDiffuse.setData(mcastMessage.toString().getBytes());
-				socket.send(msgToDiffuse);
-				
-				System.out.println(mcastMessage.toString() + " " + i);
-				
-				//Just for avoiding overflood
-				Thread.sleep(1000);
-				i++;
-			}
-		} catch (SocketException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
+			TimerTask t1 = new TimerTask() {
+				@Override
+				public void run() {
+					advertiser(servicePort, multicastAddress, multicastPort);
+				}
+			};
+			t1.run();
+
+			TimerTask t2 = new TimerTask() {
+				@Override
+				public void run() {
+					requestsProcessor();
+				}
+			};
+		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 	}
