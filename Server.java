6b072b89c33c471107b4c386ed8992c0d2667381
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-02-28 11:44:34 +0000

    Server requests processing done

--- Server.java
+++ Server.java
@@ -1,6 +1,7 @@
 package SDIS;
 
 import java.io.IOException;
+import java.io.StreamCorruptedException;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.TimerTask;
@@ -48,9 +49,57 @@ public class Server {
 		try {
 			MulticastSocket socket = generateSocket();
 			byte[] data = new byte[UDP_DATAGRAM_MAX_LENGTH];
+			DatagramPacket msgReceived = new DatagramPacket(data, data.length);
 
 			while(true) {
+				socket.receive(msgReceived);
+				String msgText = new String(data, 0, msgReceived.getLength());
+				System.out.println(msgText);
 
+				//Prepare the response
+				InetAddress clientAddress = msgReceived.getAddress();
+				int clientPort = msgReceived.getPort();
+				DatagramPacket msgToSend = new DatagramPacket(data, data.length, clientAddress, clientPort);
+				String response = "";
+
+				String splittedMsg[] = msgText.split(" ");
+				String oper = splittedMsg[0];
+
+				if(oper.equalsIgnoreCase("register") && splittedMsg.length == 3) {
+					String plateNumber = splittedMsg[1];
+					StringBuilder ownerName = new StringBuilder(splittedMsg[2]);
+					for (int i = 3; i < splittedMsg.length; i++)
+						ownerName.append(" ").append(splittedMsg[i]);
+					Plate p = new Plate(plateNumber, ownerName.toString());
+					if(plateList.contains(p)) {
+						response = "-1 \nALREADY EXISTS";
+					}
+					else if(p.getPlateNumber().equalsIgnoreCase("INVALID")) {
+						response = "-1 \nINVALID PLATE. Format XX-XX-XX. X = [A-Z0-9]";
+					}
+					else {
+						plateList.add(p);
+						response = Integer.toString(plateList.size());
+					}
+				}
+				else if(oper.equalsIgnoreCase("lookup") && splittedMsg.length == 2) {
+					String plateNumber = splittedMsg[1];
+					boolean found = false;
+					for(Plate p : plateList) {
+						if(p.getPlateNumber().equals(plateNumber)) {
+							found = true;
+							response = p.getOwnerName();
+						}
+					}
+					if(!found)
+						response = "NOT_FOUND";
+				}
+				else {
+					continue;	// Ignore malformed messages
+				}
+				msgToSend.setData(response.getBytes());
+				socket.send(msgToSend);
+				System.out.println(response);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
