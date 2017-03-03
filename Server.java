c73cf41c8ce3839b8bdc61c9bca90b25fe41b1f8
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-03-03 20:21:48 +0000

    Server is now able to serve concurrent client requests

--- Server.java
+++ Server.java
@@ -1,103 +1,102 @@
 package SDIS;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.*;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Server {
+    private static ArrayList<Plate> plateList = new ArrayList<>();
 
-	private static void requestsProcessor(int serverPort) {
-		ArrayList<Plate> plateList = new ArrayList<>();
+    private static void dispatcher(int serverPort) {
+        try {
+            ServerSocket receivingSocket = new ServerSocket(serverPort);
+            while (true) {
+                Socket connectionSocket = receivingSocket.accept();
+                Timer timerReq = new Timer();
+                TimerTask t = new TimerTask() {
+                    @Override
+                    public void run() {
+                        requestsProcessor(connectionSocket);
+                    }
+                };
+                timerReq.schedule(t, 0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Prevent process lock if a TimerTask is running
+            System.exit(1);
+        }
+    }
 
-		try {
-			ServerSocket receivingSocket = new ServerSocket(serverPort);
+    private static void requestsProcessor(Socket connectionSocket) {
+        try {
+            connectionSocket.setSoTimeout(3000); //3 sec
+            InputStreamReader istreamReader = new InputStreamReader(connectionSocket.getInputStream());
+            BufferedReader reader = new BufferedReader(istreamReader);
+            String msgRcvText = reader.readLine();
 
-			while(true) {
-				Socket connectionSocket = receivingSocket.accept();
-				connectionSocket.setSoTimeout(3000); //3 sec
-				InputStreamReader istreamReader = new InputStreamReader(connectionSocket.getInputStream());
-				BufferedReader reader = new BufferedReader(istreamReader);
-				String msgRcvText = reader.readLine();
+            //Prepare the response
+            String response = "";
 
-				//Prepare the response
-				String response = "";
+            String splittedMsg[] = msgRcvText.split(" ");
+            String oper = splittedMsg[0];
 
-				String splittedMsg[] = msgRcvText.split(" ");
-				String oper = splittedMsg[0];
+            if (oper.equalsIgnoreCase("register") && splittedMsg.length == 3) {
+                String plateNumber = splittedMsg[1];
+                StringBuilder ownerName = new StringBuilder(splittedMsg[2]);
+                for (int i = 3; i < splittedMsg.length; i++)
+                    ownerName.append(" ").append(splittedMsg[i]);
+                Plate p = new Plate(plateNumber, ownerName.toString());
+                if (plateList.contains(p)) {
+                    response = "-1 \nALREADY EXISTS";
+                } else if (p.getPlateNumber().equalsIgnoreCase("INVALID")) {
+                    response = "-1 \nINVALID PLATE. Format XX-XX-XX. X = [A-Z0-9]";
+                } else {
+                    plateList.add(p);
+                    response = Integer.toString(plateList.size());
+                }
+            } else if (oper.equalsIgnoreCase("lookup") && splittedMsg.length == 2) {
+                String plateNumber = splittedMsg[1];
+                boolean found = false;
+                for (Plate p : plateList) {
+                    if (p.getPlateNumber().equals(plateNumber)) {
+                        found = true;
+                        response = p.getOwnerName();
+                    }
+                }
+                if (!found)
+                    response = "NOT_FOUND";
+            } else {
+                // Ignore malformed messages
+            }
 
-				if(oper.equalsIgnoreCase("register") && splittedMsg.length == 3) {
-					String plateNumber = splittedMsg[1];
-					StringBuilder ownerName = new StringBuilder(splittedMsg[2]);
-					for (int i = 3; i < splittedMsg.length; i++)
-						ownerName.append(" ").append(splittedMsg[i]);
-					Plate p = new Plate(plateNumber, ownerName.toString());
-					if(plateList.contains(p)) {
-						response = "-1 \nALREADY EXISTS";
-					}
-					else if(p.getPlateNumber().equalsIgnoreCase("INVALID")) {
-						response = "-1 \nINVALID PLATE. Format XX-XX-XX. X = [A-Z0-9]";
-					}
-					else {
-						plateList.add(p);
-						response = Integer.toString(plateList.size());
-					}
-				}
-				else if(oper.equalsIgnoreCase("lookup") && splittedMsg.length == 2) {
-					String plateNumber = splittedMsg[1];
-					boolean found = false;
-					for(Plate p : plateList) {
-						if(p.getPlateNumber().equals(plateNumber)) {
-							found = true;
-							response = p.getOwnerName();
-						}
-					}
-					if(!found)
-						response = "NOT_FOUND";
-				}
-				else {
-					continue;	// Ignore malformed messages
-				}
+            OutputStream ostream = connectionSocket.getOutputStream();
+            PrintWriter prtWriter = new PrintWriter(ostream, true); //True for flushing the buffer
+            prtWriter.println(response);
 
-				OutputStream ostream = connectionSocket.getOutputStream();
-				PrintWriter prtWriter = new PrintWriter(ostream, true); //True for flushing the buffer
-				prtWriter.println(response);
+            istreamReader.close();
+            ostream.close();
+            connectionSocket.close();
+            System.out.println(msgRcvText + " : " + response);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-				istreamReader.close();
-				ostream.close();
-				connectionSocket.close();
-				System.out.println(msgRcvText + " : " + response);
-			}
-		} catch (SocketException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Server <srvc_port>");
+            return;
+        }
 
-	public static void main(String[] args) {
-		if(args.length != 1) {
-			System.out.println("Usage: java Server <srvc_port>");
-			return;
-		}
-
-		int servicePort = Integer.parseInt(args[0]);
-		
-		Timer timerReq = new Timer("requestProcessor");
-
-		TimerTask t2 = new TimerTask() {
-			@Override
-			public void run() {
-				requestsProcessor(servicePort);
-			}
-		};
-		
-		timerReq.schedule(t2, 0);
-	}
+        int servicePort = Integer.parseInt(args[0]);
+        dispatcher(servicePort);
+    }
 
 }
