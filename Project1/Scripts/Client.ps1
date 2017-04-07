e1a48b007e5213a748f1fe3e43d67e33c04ab28c
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-04-07 16:29:55 +0100

    Scripts updated for packages

--- Project1/Scripts/Client.ps1
+++ Project1/Scripts/Client.ps1
@@ -1,4 +1,7 @@
-javac Client.java
-java Client localhost:2005 restore abc.txt
+cd ..
+cd ..
+javac Project1/Client/Client.java Project1/Server/ServerRMI.java
+java Project1/Client/Client localhost:2005 restore abc.txt
+cd Project1/Client
 rm *.class
 pause
--- Project1/Scripts/Server.ps1
+++ Project1/Scripts/Server.ps1
@@ -1,4 +1,7 @@
-javac Server.java
-java Server 1.0 1 localhost:2005 224.0.0.243 2500 224.0.0.213 3000 224.0.0.251 6000
+cd ..
+cd ..
+javac Project1/Server/Server.java
+java Project1/Server/Server 1.0 1 localhost:2005 224.0.0.243 2500 224.0.0.213 3000 224.0.0.251 6000
+cd Project1/Server
 rm *.class
 pause
