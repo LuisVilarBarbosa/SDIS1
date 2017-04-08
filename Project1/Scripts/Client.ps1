e861b0fc06cdcfda064cb443fa868f5181742dd2
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-04-08 22:31:47 +0100

    Powershell client script bug fix

--- Project1/Scripts/Client.ps1
+++ Project1/Scripts/Client.ps1
@@ -5,7 +5,7 @@ javac Project1/Client/Client.java Project1/Server/ServerRMI.java
 $subprotocol = Read-Host -Prompt "Subprotocol"
 $filePath = Read-Host -Prompt "File path"
 echo "Running..."
-java Project1/Client/Client localhost:2005 restore abc.txt
+java Project1/Client/Client localhost:2005 $subprotocol $filePath
 cd Project1/Client
 echo "Removing .class files..."
 rm *.class
