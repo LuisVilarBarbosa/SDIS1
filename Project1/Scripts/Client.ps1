05efec828a9ae4d4e3d8dde905e62bf2d232175f
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-04-08 21:59:50 +0100

    Powershell scripts more dynamic

--- Project1/Scripts/Client.ps1
+++ Project1/Scripts/Client.ps1
@@ -1,7 +1,12 @@
 cd ..
 cd ..
+echo "Compiling..."
 javac Project1/Client/Client.java Project1/Server/ServerRMI.java
+$subprotocol = Read-Host -Prompt "Subprotocol"
+$filePath = Read-Host -Prompt "File path"
+echo "Running..."
 java Project1/Client/Client localhost:2005 restore abc.txt
 cd Project1/Client
+echo "Removing .class files..."
 rm *.class
 pause
--- Project1/Scripts/Server.ps1
+++ Project1/Scripts/Server.ps1
@@ -1,7 +1,12 @@
 cd ..
 cd ..
+echo "Compiling..."
 javac Project1/Server/Server.java
-java Project1/Server/Server 1.0 1 localhost:2005 224.0.0.243 2500 224.0.0.213 3000 224.0.0.251 6000
+$protVer = Read-Host -Prompt "Protocol version"
+$serverId = Read-Host -Prompt "Server id"
+echo "Running..."
+java Project1/Server/Server $protVer $serverId localhost:2005 224.0.0.243 2500 224.0.0.213 3000 224.0.0.251 6000
 cd Project1/Server
+echo "Removing .class files..."
 rm *.class
 pause
