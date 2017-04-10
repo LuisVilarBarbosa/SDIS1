d0d0eede0e512beb5ab1bb67079374b12cb862c1
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-04-10 11:23:45 +0100

    Not committed 'Server.ps1' script update

--- Project1/Scripts/Server.ps1
+++ Project1/Scripts/Server.ps1
@@ -1,13 +1,9 @@
 cd ..
 cd ..
-echo "Compiling..."
-javac Project1/Server/Server.java
 $protVer = Read-Host -Prompt "Protocol version"
 $serverId = Read-Host -Prompt "Server id"
 $serverPort = Read-Host -Prompt "Server port"
 echo "Running..."
 java Project1/Server/Server $protVer $serverId localhost:$serverPort 224.0.0.243 2500 224.0.0.213 3000 224.0.0.251 6000
-cd Project1/Server
-echo "Removing .class files..."
-rm *.class
+cd Project1/Scripts
 pause
