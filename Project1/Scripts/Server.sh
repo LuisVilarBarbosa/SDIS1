b48438942f5dd373fe0cdb24ce65a31fa0262d93
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-04-22 23:28:39 +0100

    Bug fix on 'Server.sh'
    This error was found in the first presentations class of April 21, 2017,
    when preparing to present. We are going to present in the second
    presentations class.

--- Project1/Scripts/Server.sh
+++ Project1/Scripts/Server.sh
@@ -8,7 +8,7 @@ read -r serverId
 echo "Server port: "
 read -r serverPort
 echo "Running..."
-java Project1/Server/Server $protVer $serverId localhost:$serverId 224.0.0.243 2500 224.0.0.213 3000 224.0.0.251 6000
+java Project1/Server/Server $protVer $serverId localhost:$serverPort 224.0.0.243 2500 224.0.0.213 3000 224.0.0.251 6000
 cd Project1/Scripts || exit
 echo "Press Enter to continue...: "
 read -r enter
